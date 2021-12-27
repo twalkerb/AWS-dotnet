729a30891de6597413ac666389c55c58cb113b7b
bryanyl <bryanyingling0@gmail.com>
2021-12-27 13:02:32 +0530

    Created a lambda function

---
+++ Lambda/test/Lambda.Tests/FunctionTest.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Xunit;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.TestUtilities;
+
+using Lambda;
+
+namespace Lambda.Tests
+{
+    public class FunctionTest
+    {
+        [Fact]
+        public void TestToUpperFunction()
+        {
+
+            // Invoke the lambda function and confirm the string was upper cased.
+            var function = new Function();
+            var context = new TestLambdaContext();
+            var upperCase = function.FunctionHandler("hello world", context);
+
+            Assert.Equal("HELLO WORLD", upperCase);
+            Console.ReadLine();
+        }
+    }
+}
