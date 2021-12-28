c560a8d6f5c620881eac4b648277fd6d0f0569e5
bryanyl <bryanyingling0@gmail.com>
2021-12-28 12:35:18 +0530

    Lambda-SNS

---
+++ Lambda-Functions/BasicSNS/src/BasicSNS/Function.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Amazon.Lambda.Core;
+using Amazon.Lambda.SNSEvents;
+
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace BasicSNS
+{
+    public class Function
+    {
+        /// <summary>
+        /// Default constructor. This constructor is used by Lambda to construct the instance. When invoked in a Lambda environment
+        /// the AWS credentials will come from the IAM role associated with the function and the AWS region will be set to the
+        /// region the Lambda function is executed in.
+        /// </summary>
+        public Function()
+        {
+
+        }
+
+
+        /// <summary>
+        /// This method is called for every Lambda invocation. This method takes in an SNS event object and can be used 
+        /// to respond to SNS messages.
+        /// </summary>
+        /// <param name="evnt"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public async Task FunctionHandler(string input, ILambdaContext context)
+        {
+            var snsEvent = new SNSEvent
+            {
+                Records = new List<SNSEvent.SNSRecord>
+                {
+                    new SNSEvent.SNSRecord
+                    {
+                        Sns = new SNSEvent.SNSMessage()
+                        {
+                            Message = "Did you get my message?"
+                        }
+                    }
+                }
+            };
+
+
+            foreach (var record in snsEvent.Records)
+            {
+                await ProcessRecordAsync(record, context);
+            }
+        }
+
+        private async Task ProcessRecordAsync(SNSEvent.SNSRecord record, ILambdaContext context)
+        {
+            context.Logger.LogLine($"Processed record {record.Sns.Message}");
+
+            // TODO: Do interesting work based on the new message
+            await Task.CompletedTask;
+        }
+    }
+}
---
+++ Lambda-Functions/BasicSNS/test/BasicSNS.Tests/FunctionTest.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Xunit;
+using Amazon.Lambda.TestUtilities;
+using Amazon.Lambda.SNSEvents;
+
+using BasicSNS;
+
+namespace BasicSNS.Tests
+{
+    public class FunctionTest
+    {
+        [Fact]
+        public async Task TestSQSEventLambdaFunction()
+        {
+            var snsEvent = new SNSEvent
+            {
+                Records = new List<SNSEvent.SNSRecord>
+                {
+                    new SNSEvent.SNSRecord
+                    {
+                        Sns = new SNSEvent.SNSMessage()
+                        {
+                            Message = "foobar"
+                        }
+                    }
+                }
+            };
+
+            var logger = new TestLambdaLogger();
+            var context = new TestLambdaContext
+            {
+                Logger = logger
+            };
+
+            var function = new Function();
+            var input = "Execute";
+            await function.FunctionHandler(input, context);
+
+            Assert.Contains("Processed record foobar", logger.Buffer.ToString());
+        }
+    }
+}
