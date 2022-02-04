60ace013f8bb6e2778f84e290bf05c0c565a95ee
bryanyl <bryanyingling0@gmail.com>
2022-02-04 20:25:39 +0530

    removed deploy files

--- Serverless/SampleMigration/aws_deploy.ps1
+++
@@ -1,9 +0,0 @@
-$functionName="xxxxxxxxxxxxxxxx-lambda"
-$zipName="xxxxxxxxxxxxxxxx.zip"
-
-Push-Location $PSScriptRoot
-$project=Split-Path -Path $PSScriptRoot -Leaf
-dotnet lambda package --configuration Release --framework netcoreapp3.1
-aws s3 cp "bin/Release/netcoreapp3.1/$project.zip" "s3://xxxxxxxxxxxxxxxx/$zipName"
-aws lambda update-function-code --function-name  $functionName --s3-bucket xxxxxxxxxxxxxxxx --s3-key $zipName
-Pop-Location
--- Serverless/SampleMigration/dotnet_deploy.ps1
+++
@@ -1,7 +0,0 @@
-$functionName="samplemigration-lambda"
-$zipName="samplemigration.zip"
-
-Push-Location $PSScriptRoot
-$project=Split-Path -Path $PSScriptRoot -Leaf
-dotnet lambda deploy-serverless xxxxxxxxxxxxxxxx --s3-bucket xxxx----xxxx----xxxx --template serverless.template --region us-east-2
-Pop-Location
