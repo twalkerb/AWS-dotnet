108af622b1aeecab0b7420d33d4c625a00d634cb
bryanyl <bryanyingling0@gmail.com>
2022-02-04 14:50:30 +0530

    serverless path

--- Serverless/SampleMigration/LambdaEntryPoint.cs
+++ Serverless/SampleMigration/LambdaEntryPoint.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Amazon.Lambda.AspNetCoreServer;
 
 namespace SampleMigration
