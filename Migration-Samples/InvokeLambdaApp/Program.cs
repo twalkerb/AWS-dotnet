fbdc70724c43f721039242392dfe5b101ceda55c
bryanyl <bryanyingling0@gmail.com>
2022-02-10 16:13:23 +0530

    directory for sample migrations

---
+++ Migration-Samples/InvokeLambdaApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InvokeLambdaApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
